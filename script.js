4c38f5430dc6743256abd1a06b98e197c11f48e9
codyzazulak1 <codyzazulak1@gmail.com>
2014-01-14 04:44:26 +0000

    DONE

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-    var searchTerm = prompt("Before we start, search for a tag!")
-    var url = 'https://api.instagram.com/v1/tags/' + searchTerm + '/media/recent?access_token=5509571.f59def8.d71968f130404523aacd18b0e3123793&callback=?';
+var searchTerm = prompt("Before we start, search for a tag!")
+var url = 'https://api.instagram.com/v1/tags/' + searchTerm + '/media/recent?access_token=5509571.f59def8.d71968f130404523aacd18b0e3123793&callback=?';
     $.getJSON(url, function(data) {
-        for (var i=0; i < 10; i++) {
+        for (var i=0; i < 20; i++) {
             $("#photobox").append("<img src=' " + data.data[i].images.standard_resolution.url + " '>");
         }
         $("#photobox img").hide();
@@ -10,7 +10,6 @@
 $(document).ready(function() {
         $('.button').click(function() {
             $('#photobox :first-child').fadeIn();
-
             setInterval(function(){
                 $('#photobox :first-child')
                 .fadeOut()
@@ -19,8 +18,7 @@ $(document).ready(function() {
                 .end()
                 .appendTo('#photobox');
                 console.log(searchTerm);
-            }, 3000);
-    
-    });
+            }, 3000); 
+        });
             
 });
